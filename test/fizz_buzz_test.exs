60c36d54ccf9edebc36a2c5bed003d5ad95a0367
Dunya Kirkali <dunyakirkali@gmail.com>
2020-03-15 23:18:48 +0100

    Implement using propcheck

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -1,8 +1,42 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
+  use PropCheck
+
   doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  property "neither" do
+    forall x <- integer(1, 1000) do
+      if rem(x, 5) == 0 or rem(x, 3) == 0 do
+        true
+      else
+        FizzBuzz.check(x) == Integer.to_string(x)
+      end
+    end
+  end
+
+  property "fizz" do
+    forall x <- integer(1, 1000) do
+      if rem(x, 5) == 0 do
+        true
+      else
+        FizzBuzz.check(x * 3) == "Fizz"
+      end
+    end
+  end
+
+  property "buzz" do
+    forall x <- integer(1, 1000) do
+      if rem(x, 3) == 0 do
+        true
+      else
+        FizzBuzz.check(x * 5) == "Buzz"
+      end
+    end
+  end
+
+  property "fizz_buzz" do
+    forall x <- integer(1, 1000) do
+      FizzBuzz.check(x * 15) == "FizzBuzz"
+    end
   end
 end
